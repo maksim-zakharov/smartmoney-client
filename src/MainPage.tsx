f72b367be2c22af2c609c7a07e4041ada87da461
m.s.zakharov <m.s.zakharov@tbank.ru>
2024-12-24 20:21:15 +0300

    график доходности

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -922,7 +922,7 @@ const MainPage: React.FC = () => {
             }
         ];
 
-        const minDate = moment('2024-12-19T00:00:00.000Z');
+        const minDate = moment('2024-12-23T00:00:00.000Z');
         const min = minDate.unix()
         const max = moment().unix()
 
@@ -932,6 +932,29 @@ const MainPage: React.FC = () => {
         });
 
         const filteredHistory = history.filter(c => moment(c.limitTrade?.date).unix() >= fromDate && moment(c.limitTrade?.date).unix() <= toDate);
+
+    const NametotalPnL = useMemo(() => filteredHistory.filter(p => p.PnL).reduce((acc, curr) => {
+        if (!acc[curr.pattern]) {
+            acc[curr.pattern] = 0;
+        }
+        acc[curr.pattern] += curr.PnL;
+        return acc;
+    }, {}), [filteredHistory])
+    const Namelosses = useMemo(() => filteredHistory.filter(p => p.PnL < 0).reduce((acc, curr) => {
+        if (!acc[curr.pattern]) {
+            acc[curr.pattern] = 0;
+        }
+        acc[curr.pattern]++;
+        return acc;
+    }, {}), [filteredHistory])
+    const Nameprofits = useMemo(() => filteredHistory.filter(p => p.PnL > 0).reduce((acc, curr) => {
+        if (!acc[curr.pattern]) {
+            acc[curr.pattern] = 0;
+        }
+        acc[curr.pattern]++;
+        return acc;
+    }, {}), [filteredHistory])
+
         const totalPnL = useMemo(() => filteredHistory.filter(p => p.PnL).reduce((acc, curr) => {
             if (!acc[curr.timeframe]) {
                 acc[curr.timeframe] = 0;
@@ -955,6 +978,8 @@ const MainPage: React.FC = () => {
         }, {}), [filteredHistory])
 
         const timeframes = useMemo(() => Array.from(new Set(filteredHistory.filter(p => p.timeframe).map(p => p.timeframe))).sort((a, b) => a - b), [filteredHistory]);
+
+        const names = useMemo(() => Array.from(new Set(filteredHistory.filter(p => p.pattern).map(p => p.pattern))).sort((a, b) => a - b), [filteredHistory]);
 
         const emas = [{
             array: data.ema20, color: 'rgba(255, 0, 0, 0.65)', title: 'ema20'
@@ -984,58 +1009,92 @@ const MainPage: React.FC = () => {
             1800: 'M30'
         }
 
-        return (
-            <Space direction="vertical" style={{width: '100%'}}>
-                <div style={{margin: '0 40px'}}>
-                    <Slider range defaultValue={[fromDate, toDate]} marks={marks}
-                            onChange={([fromDate, toDate]) => setDates({fromDate, toDate})}
-                            tooltip={{formatter: val => moment(val * 1000).format('YYYY-MM-DD')}}
-                            min={min} step={60 * 60 * 24} max={moment().unix()}/>
-                </div>
-                {timeframes.map(tf =>
-                    <Row gutter={8}>
-                        <Col span={8}>
-                            <Card bordered={false}>
-                                <Statistic
-                                    title={`Общий финрез ${timeframeLabelMap[tf]}`}
-                                    value={moneyFormat(totalPnL[tf], 'RUB', 2, 2)}
-                                    precision={2}
-                                    valueStyle={{color: totalPnL[tf] > 0 ? "rgb(44, 232, 156)" : "rgb(255, 117, 132)"}}
-                                />
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card bordered={false}>
-                                <Statistic
-                                    title={`Прибыльных сделок ${timeframeLabelMap[tf]}`}
-                                    value={profits[tf]}
-                                    valueStyle={{color: "rgb(44, 232, 156)"}}
-                                    suffix={`(${!profits[tf] ? 0 : (profits[tf] * 100 / ((profits[tf] || 0) + (losses[tf] || 0))).toFixed(2)})%`}
-                                />
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card bordered={false}>
-                                <Statistic
-                                    title={`Убыточных сделок ${timeframeLabelMap[tf]}`}
-                                    value={losses[tf]}
-                                    valueStyle={{color: "rgb(255, 117, 132)"}}
-                                    suffix={`(${!losses[tf] ? 0 : (losses[tf] * 100 / ((profits[tf] || 0) + (losses[tf] || 0))).toFixed(2)})%`}
-                                />
-                            </Card>
-                        </Col>
-                    </Row>
-                )}
-                <div style={{display: 'grid', gridTemplateColumns: 'auto 1300px', gap: '8px'}}>
-                    <ChartComponent {...props} data={candles} emas={emas} stop={stop} take={take} tf={tf}
-                                    markers={markers}
-                                    orderBlock={orderBlock}
-                                    imbalance={imbalance}
-                                    position={position}/>
-                    <Tabs defaultActiveKey="positions" activeKey={tab} items={items} onChange={onChange}/>
-                </div>
-            </Space>
-        );
+    return (
+        <Space direction="vertical" style={{width: '100%'}}>
+            <div style={{margin: '0 40px'}}>
+                <Slider range defaultValue={[fromDate, toDate]} marks={marks}
+                        onChange={([fromDate, toDate]) => setDates({fromDate, toDate})}
+                        tooltip={{formatter: val => moment(val * 1000).format('YYYY-MM-DD')}}
+                        min={min} step={60 * 60 * 24} max={moment().unix()}/>
+            </div>
+            {names.map(name =>
+                <Row gutter={8}>
+                    <Col span={8}>
+                        <Card bordered={false}>
+                            <Statistic
+                                title={`Общий финрез ${name}`}
+                                value={moneyFormat(NametotalPnL[name], 'RUB', 2, 2)}
+                                precision={2}
+                                valueStyle={{color: NametotalPnL[name] > 0 ? "rgb(44, 232, 156)" : "rgb(255, 117, 132)"}}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card bordered={false}>
+                            <Statistic
+                                title={`Прибыльных сделок ${name}`}
+                                value={Nameprofits[name]}
+                                valueStyle={{color: "rgb(44, 232, 156)"}}
+                                suffix={`(${!Nameprofits[name] ? 0 : (Nameprofits[name] * 100 / ((Nameprofits[name] || 0) + (Namelosses[name] || 0))).toFixed(2)})%`}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card bordered={false}>
+                            <Statistic
+                                title={`Убыточных сделок ${name}`}
+                                value={Namelosses[name]}
+                                valueStyle={{color: "rgb(255, 117, 132)"}}
+                                suffix={`(${!Namelosses[name] ? 0 : (Namelosses[name] * 100 / ((Nameprofits[name] || 0) + (Namelosses[name] || 0))).toFixed(2)})%`}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            )}
+            {timeframes.map(tf =>
+                <Row gutter={8}>
+                    <Col span={8}>
+                        <Card bordered={false}>
+                            <Statistic
+                                title={`Общий финрез ${timeframeLabelMap[tf]}`}
+                                value={moneyFormat(totalPnL[tf], 'RUB', 2, 2)}
+                                precision={2}
+                                valueStyle={{color: totalPnL[tf] > 0 ? "rgb(44, 232, 156)" : "rgb(255, 117, 132)"}}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card bordered={false}>
+                            <Statistic
+                                title={`Прибыльных сделок ${timeframeLabelMap[tf]}`}
+                                value={profits[tf]}
+                                valueStyle={{color: "rgb(44, 232, 156)"}}
+                                suffix={`(${!profits[tf] ? 0 : (profits[tf] * 100 / ((profits[tf] || 0) + (losses[tf] || 0))).toFixed(2)})%`}
+                            />
+                        </Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card bordered={false}>
+                            <Statistic
+                                title={`Убыточных сделок ${timeframeLabelMap[tf]}`}
+                                value={losses[tf]}
+                                valueStyle={{color: "rgb(255, 117, 132)"}}
+                                suffix={`(${!losses[tf] ? 0 : (losses[tf] * 100 / ((profits[tf] || 0) + (losses[tf] || 0))).toFixed(2)})%`}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+            )}
+            <div style={{display: 'grid', gridTemplateColumns: 'auto 1300px', gap: '8px'}}>
+                <ChartComponent {...props} data={candles} emas={emas} stop={stop} take={take} tf={tf}
+                                markers={markers}
+                                orderBlock={orderBlock}
+                                imbalance={imbalance}
+                                position={position}/>
+                <Tabs defaultActiveKey="positions" activeKey={tab} items={items} onChange={onChange}/>
+            </div>
+        </Space>
+    );
     }
 ;
 export const summ = (numbers: number[]) =>
