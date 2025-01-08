518def596f6d51fd52843ba2f8cea17bc86b7d39
Maksim Zakharov <max89701@gmail.com>
2025-01-08 16:19:56 +0300

    all pages demo

--- src/DemoPage/pages/EntryPage.tsx
+++ src/DemoPage/pages/EntryPage.tsx
@@ -1,4 +1,20 @@
 import {Typography} from "antd";
+import img31 from "../../assets/img_31.png"
+import img32 from "../../assets/img_32.png"
+import img33 from "../../assets/img_33.png"
+import img34 from "../../assets/img_34.png"
+import img35 from "../../assets/img_35.png"
+import img36 from "../../assets/img_36.png"
+import img37 from "../../assets/img_37.png"
+import img38 from "../../assets/img_38.png"
+import img39 from "../../assets/img_39.png"
+import img40 from "../../assets/img_40.png"
+import img41 from "../../assets/img_41.png"
+import img42 from "../../assets/img_42.png"
+import img43 from "../../assets/img_43.png"
+import img44 from "../../assets/img_44.png"
+import img45 from "../../assets/img_45.png"
+import img46 from "../../assets/img_46.png"
 
 const EntryPage = () => {
 
@@ -10,8 +26,8 @@ const EntryPage = () => {
             different different types of Entry Module Like CHoCH / BOS / FLiP / Single Candle Mitigation
             Entries . Let see What Happen in Lower time frame during entries .
         </Typography.Paragraph>
-        <img src="/public/img_31.png"/>
-        <img src="/public/img_32.png"/>
+        <img src={img31}/>
+        <img src={img32}/>
         <Typography.Paragraph>
             Flip Entry Module are very common and powerful method in smart money concept . when
             Price tap on HTF Supply Zone then you have to switch into Lower time frame as per our
@@ -21,8 +37,8 @@ const EntryPage = () => {
             convert into supply D2S or Supply Convert into Demand S2D . Lets understand by given
             diagram and Example .
         </Typography.Paragraph>
-        <img src="/public/img_33.png"/>
-        <img src="/public/img_34.png"/>
+        <img src={img33}/>
+        <img src={img34}/>
         <Typography.Paragraph>
             CHoCH is first indication for reversal and you'll see two type entry during Change of
             character first one is CHoCH with inducement and second entry CHoCH Without inducement
@@ -30,16 +46,16 @@ const EntryPage = () => {
             enter as per our entry module and same thing applicable in bullish and bearish condition. lets
             understand by given diagrams and examples .
         </Typography.Paragraph>
-        <img src="/public/img_35.png"/>
-        <img src="/public/img_36.png"/>
+        <img src={img35}/>
+        <img src={img36}/>
         <Typography.Paragraph>
             When Price tap on Higher time frame POI and give small reaction in terms of structure and
             recet Order Block and give small reaction and failed to hold supply and finally break and
             create new demand / OB then you have to wait for IDM or Demand sweep for buy
             confirmation because liquidity is important . after taken out Liquidity we can Buy / Sell Here .
         </Typography.Paragraph>
-        <img src="/public/img_37.png"/>
-        <img src="/public/img_38.png"/>
+        <img src={img37}/>
+        <img src={img38}/>
         <Typography.Paragraph>
             if you don't know that how do scale you're Winner and cut your'e losses then SMC not for you
             .Single candle mitigation is very Powerful way to add multiple entries in you're winside . single
@@ -47,16 +63,16 @@ const EntryPage = () => {
             you have to identify POI when you can add entries like this , Now i am going to explaining
             here That how and where you can trade trade using sweep and single candle mitigation.
         </Typography.Paragraph>
-        <img src="/public/img_39.png"/>
-        <img src="/public/img_40.png"/>
+        <img src={img39}/>
+        <img src={img40}/>
         <Typography.Paragraph>
             Sweep and Single candle mitigation mostly work on similar Liquidity . Taken out Major High
             and Low thats is called Liquidity Sweep and Taken our Prev Candle High and Low that's
             called Single Candle Mitigation Entry . and You can also take entry on single candle Order
             Block based . Price must be tap on LTF POI then you can take Single candle Mitigation entry.
         </Typography.Paragraph>
-        <img src="/public/img_41.png"/>
-        <img src="/public/img_42.png"/>
+        <img src={img41}/>
+        <img src={img42}/>
         <Typography.Paragraph>
             When you start play on both side move major and minor thats is Ping Pong Entries . These
             types of entries are really toughest because few peoples can think in both side and main
@@ -69,8 +85,8 @@ const EntryPage = () => {
             market tap on your POI then identify on which level can market react and how you'll react if
             same thing happen as you planned . lets understand by few examples below .
         </Typography.Paragraph>
-        <img src="/public/img_43.png"/>
-        <img src="/public/img_44.png"/>
+        <img src={img43}/>
+        <img src={img44}/>
         <Typography.Paragraph>
             Always remember 2 things during Entries to identify Ping Pong, First things identify structure
             and if structure is bearish then always Buy on Demand or Prev Low Sweep and this buy will be
@@ -79,8 +95,8 @@ const EntryPage = () => {
             High then then sell for short term and Buy when price taken out Low or tap on demand . this
             Buy will be High Probability Buy .
         </Typography.Paragraph>
-        <img src="/public/img_45.png"/>
-        <img src="/public/img_46.png"/>
+        <img src={img45}/>
+        <img src={img46}/>
     </>
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './dist',
+    emptyOutDir: true, // also necessary
+  },
   plugins: [react()],
   define: {
     'process.env': JSON.stringify(process.env.NODE_ENV),
