6d2380d21168c96a8265a45c2a45777cfa8987b8
Maksim Zakharov <max89701@gmail.com>
2025-06-27 00:56:24 +0300

    calculateCHoCHWithIDM

--- src/ArbitrageMOEXPage.tsx
+++ src/ArbitrageMOEXPage.tsx
@@ -1,129 +1,150 @@
-import React, {useEffect, useMemo, useState} from "react";
-import {useSearchParams} from "react-router-dom";
-import {Checkbox, DatePicker, Radio, Select, Slider, Space, TimeRangePickerProps} from "antd";
+import React, { useEffect, useMemo, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Checkbox, DatePicker, Radio, Select, Slider, Space, TimeRangePickerProps } from 'antd';
 import dayjs from 'dayjs';
 import type { Dayjs } from 'dayjs';
-import {Chart} from "./Chart";
-import {calculateCandle, symbolFuturePairs} from "../symbolFuturePairs";
-import moment from "moment";
-import {calculateMultiple, fetchCandlesFromAlor, getCommonCandles} from "./utils";
-import {TickerSelect} from "./TickerSelect";
-import {TimeframeSelect} from "./TimeframeSelect";
+import { Chart } from './Chart';
+import { calculateCandle, symbolFuturePairs } from '../symbolFuturePairs';
+import moment from 'moment';
+import { calculateMultiple, fetchCandlesFromAlor, getCommonCandles } from './utils';
+import { TickerSelect } from './TickerSelect';
+import { TimeframeSelect } from './TimeframeSelect';
 
-const {RangePicker} = DatePicker
+const { RangePicker } = DatePicker;
 
 export const ArbitrageMOEXPage = () => {
-    const [chartValues, onChangeChart] = useState({filteredBuyMarkers: [], filteredSellMarkers: []});
-    const [inputTreshold, onChange] = useState(0.006);
-    const [stockData, setStockData] = useState([]);
-    const [futureData, setFutureData] = useState([]);
-    const [searchParams, setSearchParams] = useSearchParams();
-    const tickerStock = searchParams.get('ticker-stock') || 'SBER';
-    // const tickerFuture = searchParams.get('ticker-future') || 'SBRF-12.24';
-    const tf = searchParams.get('tf') || '900';
-    const fromDate = searchParams.get('fromDate') || moment().add(-30, 'day').unix();
-    const toDate = searchParams.get('toDate') || moment().add(1, 'day').unix();
+  const [chartValues, onChangeChart] = useState({ filteredBuyMarkers: [], filteredSellMarkers: [] });
+  const [inputTreshold, onChange] = useState(0.006); // 0.6%
+  const fee = 0.0004; // 0.04%
+  const [stockData, setStockData] = useState([]);
+  const [futureData, setFutureData] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const tickerStock = searchParams.get('ticker-stock') || 'SBER';
+  const multi = Number(searchParams.get('multi'));
+  const tickerFuture = searchParams.get('ticker-future') || 'SBRF-12.24';
+  const tf = searchParams.get('tf') || '900';
+  const fromDate = searchParams.get('fromDate') || moment().add(-30, 'day').unix();
+  const toDate = searchParams.get('toDate') || moment().add(1, 'day').unix();
 
-    const tickerFuture = useMemo(() => {
-        const ticker = symbolFuturePairs.find(pair => pair.stockSymbol === tickerStock)?.futuresSymbol;
-        if(ticker){
-            return `${ticker}-12.24`
-        }
-        return ticker;
-    }, [tickerStock]);
+  // const tickerFuture = useMemo(() => {
+  //   const ticker = symbolFuturePairs.find((pair) => pair.stockSymbol === tickerStock)?.futuresSymbol;
+  //   if (ticker) {
+  //     return `${ticker}-6.25`;
+  //   }
+  //   return ticker;
+  // }, [tickerStock]);
 
+  const multiple = useMemo(
+    () =>
+      multi ??
+      (stockData?.length && futureData?.length
+        ? calculateMultiple(stockData[stockData.length - 1].close, futureData[futureData.length - 1].close)
+        : 0),
+    [futureData, stockData, multi],
+  );
 
-    const multiple = useMemo(() =>  stockData?.length && futureData?.length ? calculateMultiple(stockData[stockData.length - 1].close, futureData[futureData.length - 1].close) : 0, [futureData, stockData]);
+  const stockTickers = useMemo(() => symbolFuturePairs.map((pair) => pair.stockSymbol), []);
 
-    const stockTickers = useMemo(() => symbolFuturePairs.map(pair => pair.stockSymbol), []);
+  useEffect(() => {
+    tickerStock && fetchCandlesFromAlor(tickerStock, tf, fromDate, toDate).then(setStockData);
+  }, [tf, tickerStock, fromDate, toDate]);
 
-    useEffect(() => {
-        tickerStock && fetchCandlesFromAlor(tickerStock, tf, fromDate, toDate).then(setStockData);
-    }, [tf, tickerStock, fromDate, toDate]);
+  useEffect(() => {
+    tickerFuture && fetchCandlesFromAlor(tickerFuture, tf, fromDate, toDate).then(setFutureData);
+  }, [tf, tickerFuture, fromDate, toDate]);
 
-    useEffect(() => {
-        tickerFuture && fetchCandlesFromAlor(tickerFuture, tf, fromDate, toDate).then(setFutureData);
-    }, [tf, tickerFuture, fromDate, toDate]);
+  const commonCandles = useMemo(() => getCommonCandles(stockData, futureData), [stockData, futureData]);
 
-    const commonCandles = useMemo(() => getCommonCandles(stockData, futureData), [stockData, futureData]);
+  const data = useMemo(() => {
+    if (stockData?.length && futureData?.length) {
+      const { filteredStockCandles, filteredFuturesCandles } = getCommonCandles(stockData, futureData);
 
-    const data = useMemo(() => {
-        if(stockData?.length && futureData?.length){
-            const {filteredStockCandles, filteredFuturesCandles} = getCommonCandles(stockData, futureData);
+      return filteredFuturesCandles
+        .map((item, index) => calculateCandle(filteredStockCandles[index], item, Number(multiple)))
+        .filter(Boolean);
+    }
+    return stockData;
+  }, [stockData, futureData, multiple]);
 
-        return filteredFuturesCandles.map((item, index) => calculateCandle(filteredStockCandles[index], item, Number(multiple))).filter(Boolean)
-        }
-        return stockData;
-    }, [stockData, futureData, multiple]);
-
-    const profit = useMemo(() => {
-        let PnL = 0;
-        for (let i = 0; i < chartValues.filteredBuyMarkers.length; i++) {
-            const marker = chartValues.filteredBuyMarkers[i];
-            const stockCandle = commonCandles.filteredStockCandles[marker.index];
-            PnL += inputTreshold; // (stockCandle.close * inputTreshold);
-        }
-        for (let i = 0; i < chartValues.filteredSellMarkers.length; i++) {
-            const marker = chartValues.filteredSellMarkers[i];
-            const stockCandle = commonCandles.filteredStockCandles[marker.index];
-            PnL += inputTreshold; // (stockCandle.close * inputTreshold);
-        }
-
-        return new Intl.NumberFormat('en-US', {
-            minimumFractionDigits: 2,  // Минимальное количество знаков после запятой
-            maximumFractionDigits: 2,  // Максимальное количество знаков после запятой
-        }).format(PnL * 100);
-    }, [chartValues, inputTreshold, commonCandles])
-
-    const setSize = (tf: string) => {
-        searchParams.set('tf', tf);
-        setSearchParams(searchParams)
+  const profit = useMemo(() => {
+    let PnL = 0;
+    let buyTrades = 0;
+    let sellTrades = 0;
+    for (let i = 0; i < chartValues.filteredBuyMarkers.length; i++) {
+      const marker = chartValues.filteredBuyMarkers[i];
+      const stockCandle = commonCandles.filteredStockCandles[marker.index];
+      PnL += inputTreshold - fee * 2; // (stockCandle.close * inputTreshold);
+      buyTrades++;
+    }
+    for (let i = 0; i < chartValues.filteredSellMarkers.length; i++) {
+      const marker = chartValues.filteredSellMarkers[i];
+      const stockCandle = commonCandles.filteredStockCandles[marker.index];
+      PnL += inputTreshold - fee * 2; // (stockCandle.close * inputTreshold);
+      sellTrades++;
     }
 
-    const onSelectTicker = (type: 'stock' | 'future') => (ticker) => {
-        searchParams.set(`ticker-${type}`, ticker);
-        setSearchParams(searchParams)
-    }
+    return {
+      PnL: new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 2, // Минимальное количество знаков после запятой
+        maximumFractionDigits: 2, // Максимальное количество знаков после запятой
+      }).format(PnL * 100),
+      buyTrades,
+      sellTrades,
+    };
+  }, [chartValues, inputTreshold, commonCandles]);
 
-    const onChangeRangeDates = (value: Dayjs[], dateString) => {
-        console.log('Selected Time: ', value);
-        console.log('Formatted Selected Time: ', dateString);
+  const setSize = (tf: string) => {
+    searchParams.set('tf', tf);
+    setSearchParams(searchParams);
+  };
 
-        searchParams.set('fromDate', value[0].unix());
-        searchParams.set('toDate', value[1].unix());
-        setSearchParams(searchParams);
-    }
+  const onSelectTicker = (type: 'stock' | 'future') => (ticker) => {
+    searchParams.set(`ticker-${type}`, ticker);
+    setSearchParams(searchParams);
+  };
 
-    const rangePresets: TimeRangePickerProps['presets'] = [
-        { label: 'Последние 7 дней', value: [dayjs().add(-7, 'd'), dayjs()] },
-        { label: 'Последние 14 дней', value: [dayjs().add(-14, 'd'), dayjs()] },
-        { label: 'Последние 30 дней', value: [dayjs().add(-30, 'd'), dayjs()] },
-        { label: 'Последние 90 дней', value: [dayjs().add(-90, 'd'), dayjs()] },
-    ];
+  const onChangeRangeDates = (value: Dayjs[], dateString) => {
+    console.log('Selected Time: ', value);
+    console.log('Formatted Selected Time: ', dateString);
 
-    return <>
-        <Space>
-            <TimeframeSelect  value={tf} onChange={setSize}/>
-            <TickerSelect filterSymbols={stockTickers} value={tickerStock} onSelect={onSelectTicker('stock')}/>
-            {/*<Select*/}
-            {/*    value={tickerFuture}*/}
-            {/*    showSearch*/}
-            {/*    placeholder="Введи тикер"*/}
-            {/*    onSelect={onSelectTicker('future')}*/}
-            {/*    filterOption={(input, option) =>*/}
-            {/*        (option?.label ?? '').toLowerCase().includes(input.toLowerCase())*/}
-            {/*    }*/}
-            {/*    style={{width: 160}}*/}
-            {/*    options={options}*/}
-            {/*/>*/}
-            <RangePicker
-                presets={rangePresets}
-                value={[dayjs(Number(fromDate) * 1000), dayjs(Number(toDate) * 1000)]}
-                format="YYYY-MM-DD"
-                onChange={onChangeRangeDates} />
-            {profit}%
-        </Space>
-        <Slider value={inputTreshold} min={0.001} max={0.03} step={0.001} onChange={onChange}/>
-    <Chart inputTreshold={inputTreshold} data={data} tf={tf} onChange={onChangeChart}/>
+    searchParams.set('fromDate', value[0].unix());
+    searchParams.set('toDate', value[1].unix());
+    setSearchParams(searchParams);
+  };
+
+  const rangePresets: TimeRangePickerProps['presets'] = [
+    { label: 'Последние 7 дней', value: [dayjs().add(-7, 'd'), dayjs()] },
+    { label: 'Последние 14 дней', value: [dayjs().add(-14, 'd'), dayjs()] },
+    { label: 'Последние 30 дней', value: [dayjs().add(-30, 'd'), dayjs()] },
+    { label: 'Последние 90 дней', value: [dayjs().add(-90, 'd'), dayjs()] },
+  ];
+
+  return (
+    <>
+      <Space>
+        <TimeframeSelect value={tf} onChange={setSize} />
+        <TickerSelect filterSymbols={stockTickers} value={tickerStock} onSelect={onSelectTicker('stock')} />
+        {/*<Select*/}
+        {/*    value={tickerFuture}*/}
+        {/*    showSearch*/}
+        {/*    placeholder="Введи тикер"*/}
+        {/*    onSelect={onSelectTicker('future')}*/}
+        {/*    filterOption={(input, option) =>*/}
+        {/*        (option?.label ?? '').toLowerCase().includes(input.toLowerCase())*/}
+        {/*    }*/}
+        {/*    style={{width: 160}}*/}
+        {/*    options={options}*/}
+        {/*/>*/}
+        <RangePicker
+          presets={rangePresets}
+          value={[dayjs(Number(fromDate) * 1000), dayjs(Number(toDate) * 1000)]}
+          format="YYYY-MM-DD"
+          onChange={onChangeRangeDates}
+        />
+        {profit.PnL}% B:{profit.buyTrades} S:{profit.sellTrades}
+      </Space>
+      <Slider value={inputTreshold} min={0.001} max={0.03} step={0.001} onChange={onChange} />
+      <Chart inputTreshold={inputTreshold} data={data} tf={tf} onChange={onChangeChart} />
     </>
-}
+  );
+};
--- symbolFuturePairs.ts
+++ symbolFuturePairs.ts
@@ -1,312 +1,309 @@
-import {HistoryObject} from "./src/sm-lib/models";
+import { HistoryObject } from './src/sm-lib/models';
 
 export const symbolFuturePairs = [
-    {
-        "futuresSymbol": "SGZH",
-        "futuresShortName": "SZZ4",
-        "stockSymbol": "SGZH"
-    },
-    {
-        "futuresSymbol": "SBRF",
-        "futuresShortName": "SRZ4",
-        "stockSymbol": "SBER"
-    },
-    {
-        "futuresSymbol": "GAZR",
-        "futuresShortName": "GZZ4",
-        "stockSymbol": "GAZP"
-    },
-    {
-        "futuresSymbol": "GMKN",
-        "futuresShortName": "GKZ4",
-        "stockSymbol": "GMKN"
-    },
-    {
-        "futuresSymbol": "VTBR",
-        "futuresShortName": "VBZ4",
-        "stockSymbol": "VTBR"
-    },
-    {
-        "futuresSymbol": "YDEX",
-        "futuresShortName": "YDZ4",
-        "stockSymbol": "YDEX"
-    },
-    {
-        "futuresSymbol": "MGNT",
-        "futuresShortName": "MNZ4",
-        "stockSymbol": "MGNT"
-    },
-    {
-        "futuresSymbol": "TRNF",
-        "futuresShortName": "TNZ4",
-        "stockSymbol": "TRNFP"
-    },
-    {
-        "futuresSymbol": "VKCO",
-        "futuresShortName": "VKZ4",
-        "stockSymbol": "VKCO"
-    },
-    {
-        "futuresSymbol": "RTKM",
-        "futuresShortName": "RTZ4",
-        "stockSymbol": "RTKM"
-    },
-    {
-        "futuresSymbol": "SMLT",
-        "futuresShortName": "SSZ4",
-        "stockSymbol": "SMLT"
-    },
-    {
-        "futuresSymbol": "TCSI",
-        "futuresShortName": "TIZ4",
-        "stockSymbol": "TCSG"
-    },
-    {
-        "futuresSymbol": "MTLR",
-        "futuresShortName": "MCZ4",
-        "stockSymbol": "MTLR"
-    },
-    {
-        "futuresSymbol": "LKOH",
-        "futuresShortName": "LKZ4",
-        "stockSymbol": "LKOH"
-    },
-    {
-        "futuresSymbol": "ALRS",
-        "futuresShortName": "ALZ4",
-        "stockSymbol": "ALRS"
-    },
-    {
-        "futuresSymbol": "POSI",
-        "futuresShortName": "PSZ4",
-        "stockSymbol": "POSI"
-    },
-    {
-        "futuresSymbol": "RUAL",
-        "futuresShortName": "RLZ4",
-        "stockSymbol": "RUAL"
-    },
-    {
-        "futuresSymbol": "RNFT",
-        "futuresShortName": "RUZ4",
-        "stockSymbol": "RNFT"
-    },
-    {
-        "futuresSymbol": "MVID",
-        "futuresShortName": "MVZ4",
-        "stockSymbol": "MVID"
-    },
-    {
-        "futuresSymbol": "ISKJ",
-        "futuresShortName": "ISZ4",
-        "stockSymbol": "ABIO"
-    },
-    {
-        "futuresSymbol": "AFLT",
-        "futuresShortName": "AFZ4",
-        "stockSymbol": "AFLT"
-    },
-    {
-        "futuresSymbol": "ROSN",
-        "futuresShortName": "RNZ4",
-        "stockSymbol": "ROSN"
-    },
-    {
-        "futuresSymbol": "NLMK",
-        "futuresShortName": "NMZ4",
-        "stockSymbol": "NLMK"
-    },
-    {
-        "futuresSymbol": "LEAS",
-        "futuresShortName": "LEZ4",
-        "stockSymbol": "LEAS"
-    },
-    {
-        "futuresSymbol": "TATN",
-        "futuresShortName": "TTZ4",
-        "stockSymbol": "TATN"
-    },
-    {
-        "futuresSymbol": "MOEX",
-        "futuresShortName": "MEZ4",
-        "stockSymbol": "MOEX"
-    },
-    {
-        "futuresSymbol": "SIBN",
-        "futuresShortName": "SOZ4",
-        "stockSymbol": "SIBN"
-    },
-    {
-        "futuresSymbol": "ASTR",
-        "futuresShortName": "ASZ4",
-        "stockSymbol": "ASTR"
-    },
-    {
-        "futuresSymbol": "SOFL",
-        "futuresShortName": "S0Z4",
-        "stockSymbol": "SOFL"
-    },
-    {
-        "futuresSymbol": "PIKK",
-        "futuresShortName": "PIZ4",
-        "stockSymbol": "PIKK"
-    },
-    {
-        "futuresSymbol": "SNGR",
-        "futuresShortName": "SNZ4",
-        "stockSymbol": "SNGS"
-    },
-    {
-        "futuresSymbol": "AFKS",
-        "futuresShortName": "AKZ4",
-        "stockSymbol": "AFKS"
-    },
-    {
-        "futuresSymbol": "BANE",
-        "futuresShortName": "BNZ4",
-        "stockSymbol": "BANE"
-    },
-    {
-        "futuresSymbol": "WUSH",
-        "futuresShortName": "WUZ4",
-        "stockSymbol": "WUSH"
-    },
-    {
-        "futuresSymbol": "FLOT",
-        "futuresShortName": "FLZ4",
-        "stockSymbol": "FLOT"
-    },
-    {
-        "futuresSymbol": "SBPR",
-        "futuresShortName": "SPZ4",
-        "stockSymbol": "SBERP"
-    },
-    {
-        "futuresSymbol": "BSPB",
-        "futuresShortName": "BSZ4",
-        "stockSymbol": "BSPB"
-    },
-    {
-        "futuresSymbol": "PHOR",
-        "futuresShortName": "PHZ4",
-        "stockSymbol": "PHOR"
-    },
-    {
-        "futuresSymbol": "RASP",
-        "futuresShortName": "RAZ4",
-        "stockSymbol": "RASP"
-    },
-    {
-        "futuresSymbol": "SNGP",
-        "futuresShortName": "SGZ4",
-        "stockSymbol": "SNGSP"
-    },
-    {
-        "futuresSymbol": "KMAZ",
-        "futuresShortName": "KMZ4",
-        "stockSymbol": "KMAZ"
-    },
-    {
-        "futuresSymbol": "TATP",
-        "futuresShortName": "TPZ4",
-        "stockSymbol": "TATNP"
-    },
-    {
-        "futuresSymbol": "FESH",
-        "futuresShortName": "FEZ4",
-        "stockSymbol": "FESH"
-    },
-    {
-        "futuresSymbol": "HYDR",
-        "futuresShortName": "HYZ4",
-        "stockSymbol": "HYDR"
-    },
-    {
-        "futuresSymbol": "MTSI",
-        "futuresShortName": "MTZ4",
-        "stockSymbol": "MTSS"
-    },
-    {
-        "futuresSymbol": "FEES",
-        "futuresShortName": "FSZ4",
-        "stockSymbol": "FEES"
-    },
-    {
-        "futuresSymbol": "NOTK",
-        "futuresShortName": "NKZ4",
-        "stockSymbol": "NVTK"
-    },
-    {
-        "futuresSymbol": "MAGN",
-        "futuresShortName": "MGZ4",
-        "stockSymbol": "MAGN"
-    },
-    {
-        "futuresSymbol": "SVCB",
-        "futuresShortName": "SCZ4",
-        "stockSymbol": "SVCB"
-    },
-    {
-        "futuresSymbol": "CBOM",
-        "futuresShortName": "CMZ4",
-        "stockSymbol": "CBOM"
-    },
-    {
-        "futuresSymbol": "PLZL",
-        "futuresShortName": "PZZ4",
-        "stockSymbol": "PLZL"
-    },
-    {
-        "futuresSymbol": "SPBE",
-        "futuresShortName": "SEZ4",
-        "stockSymbol": "SPBE"
-    },
-    {
-        "futuresSymbol": "SFIN",
-        "futuresShortName": "SHZ4",
-        "stockSymbol": "SFIN"
-    },
-    {
-        "futuresSymbol": "CHMF",
-        "futuresShortName": "CHZ4",
-        "stockSymbol": "CHMF"
-    },
-    {
-        "futuresSymbol": "IRAO",
-        "futuresShortName": "IRZ4",
-        "stockSymbol": "IRAO"
-    }
-]
+  {
+    futuresSymbol: 'SGZH',
+    futuresShortName: 'SZZ4',
+    stockSymbol: 'SGZH',
+  },
+  {
+    futuresSymbol: 'SBRF',
+    futuresShortName: 'SRZ4',
+    stockSymbol: 'SBER',
+  },
+  {
+    futuresSymbol: 'GAZR',
+    futuresShortName: 'GZZ4',
+    stockSymbol: 'GAZP',
+  },
+  {
+    futuresSymbol: 'GMKN',
+    futuresShortName: 'GKZ4',
+    stockSymbol: 'GMKN',
+  },
+  {
+    futuresSymbol: 'VTBR',
+    futuresShortName: 'VBZ4',
+    stockSymbol: 'VTBR',
+  },
+  {
+    futuresSymbol: 'YDEX',
+    futuresShortName: 'YDZ4',
+    stockSymbol: 'YDEX',
+  },
+  {
+    futuresSymbol: 'MGNT',
+    futuresShortName: 'MNZ4',
+    stockSymbol: 'MGNT',
+  },
+  {
+    futuresSymbol: 'TRNF',
+    futuresShortName: 'TNZ4',
+    stockSymbol: 'TRNFP',
+  },
+  {
+    futuresSymbol: 'VKCO',
+    futuresShortName: 'VKZ4',
+    stockSymbol: 'VKCO',
+  },
+  {
+    futuresSymbol: 'RTKM',
+    futuresShortName: 'RTZ4',
+    stockSymbol: 'RTKM',
+  },
+  {
+    futuresSymbol: 'SMLT',
+    futuresShortName: 'SSZ4',
+    stockSymbol: 'SMLT',
+  },
+  {
+    futuresSymbol: 'TCSI',
+    futuresShortName: 'TIZ4',
+    stockSymbol: 'TCSG',
+  },
+  {
+    futuresSymbol: 'MTLR',
+    futuresShortName: 'MCZ4',
+    stockSymbol: 'MTLR',
+  },
+  {
+    futuresSymbol: 'LKOH',
+    futuresShortName: 'LKZ4',
+    stockSymbol: 'LKOH',
+  },
+  {
+    futuresSymbol: 'ALRS',
+    futuresShortName: 'ALZ4',
+    stockSymbol: 'ALRS',
+  },
+  {
+    futuresSymbol: 'POSI',
+    futuresShortName: 'PSZ4',
+    stockSymbol: 'POSI',
+  },
+  {
+    futuresSymbol: 'RUAL',
+    futuresShortName: 'RLZ4',
+    stockSymbol: 'RUAL',
+  },
+  {
+    futuresSymbol: 'RNFT',
+    futuresShortName: 'RUZ4',
+    stockSymbol: 'RNFT',
+  },
+  {
+    futuresSymbol: 'MVID',
+    futuresShortName: 'MVZ4',
+    stockSymbol: 'MVID',
+  },
+  {
+    futuresSymbol: 'ISKJ',
+    futuresShortName: 'ISZ4',
+    stockSymbol: 'ABIO',
+  },
+  {
+    futuresSymbol: 'AFLT',
+    futuresShortName: 'AFZ4',
+    stockSymbol: 'AFLT',
+  },
+  {
+    futuresSymbol: 'ROSN',
+    futuresShortName: 'RNZ4',
+    stockSymbol: 'ROSN',
+  },
+  {
+    futuresSymbol: 'NLMK',
+    futuresShortName: 'NMZ4',
+    stockSymbol: 'NLMK',
+  },
+  {
+    futuresSymbol: 'LEAS',
+    futuresShortName: 'LEZ4',
+    stockSymbol: 'LEAS',
+  },
+  {
+    futuresSymbol: 'TATN',
+    futuresShortName: 'TTZ4',
+    stockSymbol: 'TATN',
+  },
+  {
+    futuresSymbol: 'MOEX',
+    futuresShortName: 'MEZ4',
+    stockSymbol: 'MOEX',
+  },
+  {
+    futuresSymbol: 'SIBN',
+    futuresShortName: 'SOZ4',
+    stockSymbol: 'SIBN',
+  },
+  {
+    futuresSymbol: 'ASTR',
+    futuresShortName: 'ASZ4',
+    stockSymbol: 'ASTR',
+  },
+  {
+    futuresSymbol: 'SOFL',
+    futuresShortName: 'S0Z4',
+    stockSymbol: 'SOFL',
+  },
+  {
+    futuresSymbol: 'PIKK',
+    futuresShortName: 'PIZ4',
+    stockSymbol: 'PIKK',
+  },
+  {
+    futuresSymbol: 'SNGR',
+    futuresShortName: 'SNZ4',
+    stockSymbol: 'SNGS',
+  },
+  {
+    futuresSymbol: 'AFKS',
+    futuresShortName: 'AKZ4',
+    stockSymbol: 'AFKS',
+  },
+  {
+    futuresSymbol: 'BANE',
+    futuresShortName: 'BNZ4',
+    stockSymbol: 'BANE',
+  },
+  {
+    futuresSymbol: 'WUSH',
+    futuresShortName: 'WUZ4',
+    stockSymbol: 'WUSH',
+  },
+  {
+    futuresSymbol: 'FLOT',
+    futuresShortName: 'FLZ4',
+    stockSymbol: 'FLOT',
+  },
+  {
+    futuresSymbol: 'SBPR',
+    futuresShortName: 'SPZ4',
+    stockSymbol: 'SBERP',
+  },
+  {
+    futuresSymbol: 'BSPB',
+    futuresShortName: 'BSZ4',
+    stockSymbol: 'BSPB',
+  },
+  {
+    futuresSymbol: 'PHOR',
+    futuresShortName: 'PHZ4',
+    stockSymbol: 'PHOR',
+  },
+  {
+    futuresSymbol: 'RASP',
+    futuresShortName: 'RAZ4',
+    stockSymbol: 'RASP',
+  },
+  {
+    futuresSymbol: 'SNGP',
+    futuresShortName: 'SGZ4',
+    stockSymbol: 'SNGSP',
+  },
+  {
+    futuresSymbol: 'KMAZ',
+    futuresShortName: 'KMZ4',
+    stockSymbol: 'KMAZ',
+  },
+  {
+    futuresSymbol: 'TATP',
+    futuresShortName: 'TPZ4',
+    stockSymbol: 'TATNP',
+  },
+  {
+    futuresSymbol: 'FESH',
+    futuresShortName: 'FEZ4',
+    stockSymbol: 'FESH',
+  },
+  {
+    futuresSymbol: 'HYDR',
+    futuresShortName: 'HYZ4',
+    stockSymbol: 'HYDR',
+  },
+  {
+    futuresSymbol: 'MTSI',
+    futuresShortName: 'MTZ4',
+    stockSymbol: 'MTSS',
+  },
+  {
+    futuresSymbol: 'FEES',
+    futuresShortName: 'FSZ4',
+    stockSymbol: 'FEES',
+  },
+  {
+    futuresSymbol: 'NOTK',
+    futuresShortName: 'NKZ4',
+    stockSymbol: 'NVTK',
+  },
+  {
+    futuresSymbol: 'MAGN',
+    futuresShortName: 'MGZ4',
+    stockSymbol: 'MAGN',
+  },
+  {
+    futuresSymbol: 'SVCB',
+    futuresShortName: 'SCZ4',
+    stockSymbol: 'SVCB',
+  },
+  {
+    futuresSymbol: 'CBOM',
+    futuresShortName: 'CMZ4',
+    stockSymbol: 'CBOM',
+  },
+  {
+    futuresSymbol: 'PLZL',
+    futuresShortName: 'PZZ4',
+    stockSymbol: 'PLZL',
+  },
+  {
+    futuresSymbol: 'SPBE',
+    futuresShortName: 'SEZ4',
+    stockSymbol: 'SPBE',
+  },
+  {
+    futuresSymbol: 'SFIN',
+    futuresShortName: 'SHZ4',
+    stockSymbol: 'SFIN',
+  },
+  {
+    futuresSymbol: 'CHMF',
+    futuresShortName: 'CHZ4',
+    stockSymbol: 'CHMF',
+  },
+  {
+    futuresSymbol: 'IRAO',
+    futuresShortName: 'IRZ4',
+    stockSymbol: 'IRAO',
+  },
+];
 
+export function calculateEMA(prices, period) {
+  const alpha = 2 / (period + 1);
+  let ema = prices[0];
+  const array = [prices[0]];
 
-export function calculateEMA(
-    prices,
-    period
-) {
-    const alpha = 2 / (period + 1);
-    let ema = prices[0];
-    const array = [prices[0]];
+  for (let i = 1; i < prices.length; i++) {
+    ema = prices[i] * alpha + ema * (1 - alpha);
+    array.push(ema);
+  }
 
-    for (let i = 1; i < prices.length; i++) {
-        ema = prices[i] * alpha + ema * (1 - alpha);
-        array.push(ema);
-    }
-
-    return [ema, array];
+  return [ema, array];
 }
 
 export const calculateCandle = (stockCandle: HistoryObject, futureCandle: HistoryObject, multiple: number = 100) => {
-    if(!stockCandle){
-        return null;
-    }if(!futureCandle){
-        return null;
-    }
+  if (!stockCandle) {
+    return null;
+  }
+  if (!futureCandle) {
+    return null;
+  }
 
-    return {
-        open: stockCandle.open / futureCandle.open * multiple,
-        close: stockCandle.close / futureCandle.close * multiple,
-        high: stockCandle.high / futureCandle.high * multiple,
-        low: stockCandle.low / futureCandle.low * multiple,
-        time: stockCandle.time,
-    } as HistoryObject
-}
+  return {
+    open: (futureCandle.open / stockCandle.open) * multiple,
+    close: (futureCandle.close / stockCandle.close) * multiple,
+    high: (futureCandle.high / stockCandle.high) * multiple,
+    low: (futureCandle.low / stockCandle.low) * multiple,
+    time: stockCandle.time,
+  } as HistoryObject;
+};
