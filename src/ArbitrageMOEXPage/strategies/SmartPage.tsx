3f1a7816742b3fc4e916af6a14da27be3e9d27bf
Maksim Zakharov <max89701@gmail.com>
2025-11-03 20:29:44 +0300

    ctrader summary

--- src/ArbitrageMOEXPage/strategies/SmartPage.tsx
+++ src/ArbitrageMOEXPage/strategies/SmartPage.tsx
@@ -90,6 +90,7 @@ export const SmartPage = () => {
 
   const spanOptions = [
     { label: 1, value: 1 },
+    { label: 2, value: 2 },
     { label: 3, value: 3 },
     { label: 4, value: 4 },
     { label: 6, value: 6 },
